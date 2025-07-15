945cee35661b07ae947275a84fe71d3d792543a3
Nicolas Oulianov <nicolas.oulianov@gmail.com>
2025-07-15 21:49:47 +0200

    Moves auth pages

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,8 +1,3 @@
-import { AuthForm } from "@/components/auth/AuthForm";
-import { ConfirmEmail } from "@/components/auth/ConfirmEmail";
-import { ForgotPassword } from "@/components/auth/ForgotPassword";
-import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
-import { ResetPassword } from "@/components/auth/ResetPassword";
 import { Layout } from "@/components/layout/layout";
 import AIControlPage from "@/pages/AIControlPage";
 import AITrainingPage from "@/pages/AITrainingPage";
@@ -13,6 +8,11 @@ import ControlPage from "@/pages/ControlPage";
 import DashboardPage from "@/pages/DashboardPage";
 import NetworkPage from "@/pages/NetworkPage";
 import ViewVideoPage from "@/pages/ViewVideoPage";
+import { AuthForm } from "@/pages/auth/AuthForm";
+import { ConfirmEmail } from "@/pages/auth/ConfirmEmail";
+import { ForgotPassword } from "@/pages/auth/ForgotPassword";
+import { ProtectedRoute } from "@/pages/auth/ProtectedRoute";
+import { ResetPassword } from "@/pages/auth/ResetPassword";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
